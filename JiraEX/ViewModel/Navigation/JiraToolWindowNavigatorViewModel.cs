c48df3bfd89e9bd5e12e667f48af04410350f562
lubomyl <31207065+lubomyl@users.noreply.github.com>
2018-09-03 11:48:58 +0200

    Implementing Tool window

--- JiraEX/ViewModel/Navigation/JiraToolWindowNavigatorViewModel.cs
+++ JiraEX/ViewModel/Navigation/JiraToolWindowNavigatorViewModel.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace JiraEX.ViewModel.Navigation
 {
-    public class JiraToolWindowNavigatorViewModel
+    public class JiraToolWindowNavigatorViewModel : ViewModelBase
     {
+
+        private object _selectedView;
+        private JiraToolWindow _parent;
+
+        public JiraToolWindowNavigatorViewModel(JiraToolWindow parent)
+        {
+            this._parent = parent;
+        }
+
+        public object SelectedView
+        {
+            get { return _selectedView; }
+            set
+            {
+                this._selectedView = value;
+                OnPropertyChanged("SelectedView");
+            }
+        }
     }
 }
---
+++ JiraEX/ViewModel/ViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiraEX.ViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, args);
+            }
+        }
+    }
+}
