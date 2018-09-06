b08260c729f7fb6f5971fe81ca7d8f5a529082b9
lubomyl <31207065+lubomyl@users.noreply.github.com>
2018-09-06 21:39:28 +0200

    Implementing SprintService - to get all Sprint objects by Board

---
+++ JiraRESTClient/Service/Implementation/SprintService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JiraRESTClient.Model;
+using AtlassianConnector.Base.Implementation.DevDefined;
+
+namespace JiraRESTClient.Service.Implementation
+{
+    public class SprintService : ISprintService
+    {
+
+        private BaseService _baseService;
+
+        public SprintService()
+        {
+            this._baseService = BaseService.JiraInstance;
+        }
+
+        public Task<SprintList> GetAllSprintsOfBoardtAsync(int boardId)
+        {
+            return Task.Run(() => {
+                var resource = $"board/{boardId}/sprint/";
+
+                return this._baseService.GetAgile<SprintList>(resource);
+            });
+        }
+    }
+}
