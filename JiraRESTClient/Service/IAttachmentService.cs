f2b89437ab8b3c73d06cbca526bef7c14fe8ab40
lubomyl <31207065+lubomyl@users.noreply.github.com>
2018-09-13 08:35:06 +0200

    Implementing AttachmentService - attachment delete by id method

---
+++ JiraRESTClient/Service/IAttachmentService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiraRESTClient.Service
+{
+
+    /// <summary>
+    /// Interface providing methods to access resources connected to Attachment object from Jira app.
+    /// </summary>
+    public interface IAttachmentService
+    {
+
+        Task DeleteAttachmentByIdAsync(string attachmentId);
+
+    }
+}
---
+++ JiraRESTClient/Service/Implementation/AttachmentService.cs
@@ -0,0 +1,39 @@
+﻿using AtlassianConnector.Base.Implementation.DevDefined;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiraRESTClient.Service.Implementation
+{
+
+    /// <summary>
+    /// Concrete implementation of IAttachmentService utilizing <see cref="BaseService"/> as <see cref="IBaseService{T}"/>.
+    /// <see cref="IAttachmentService"/>
+    /// </summary>
+    public class AttachmentService : IAttachmentService
+    {
+
+        private JiraService _baseService;
+
+        public AttachmentService()
+        {
+            this._baseService = BaseService.JiraInstance;
+        }
+
+        /// <summary>
+        /// <see cref="IAttachmentService.DeleteAttachmentByIdAsync(string)"/>
+        /// </summary>
+        public Task DeleteAttachmentByIdAsync(string attachmentId)
+        {
+            return Task.Run(() =>
+            {
+                var resource = $"attachment/{attachmentId}";
+
+                this._baseService.DeleteResource(resource);
+            });
+        }
+
+    }
+}
