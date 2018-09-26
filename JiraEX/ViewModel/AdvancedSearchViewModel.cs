d18898e89c173c26ed42b3911abdd6ec0e091193
lubomyl <31207065+lubomyl@users.noreply.github.com>
2018-09-26 15:24:28 +0200

    WIP: implementing advanced search - viewmodel properties

--- JiraEX/ViewModel/AdvancedSearchViewModel.cs
+++ JiraEX/ViewModel/AdvancedSearchViewModel.cs
@@ -2,6 +2,7 @@
 using JiraRESTClient.Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,19 @@ namespace JiraEX.ViewModel
     {
 
         private IJiraToolWindowNavigatorViewModel _parent;
+
+        private Sprint _selectedSprint;
+        private User _selectedAssignee;
+        private Priority _selectedPriority;
+        private Status _selectedStatus;
+        private Project _selectedProject;
+        private string _searchText;
+
+        private ObservableCollection<Sprint> _sprintList;
+        private ObservableCollection<User> _assigneeList;
+        private ObservableCollection<Priority> _priorityList;
+        private ObservableCollection<Status> _statusList;
+        private ObservableCollection<Project> _projectList;
 
         public AdvancedSearchViewModel(JiraToolWindowNavigatorViewModel parent)
         {
@@ -26,6 +40,116 @@ namespace JiraEX.ViewModel
         public void SetPanelTitles()
         {
             this._parent.SetPanelTitles("JiraEX", "Advanced search");
+        }
+
+        public Sprint SelectedSprint
+        {
+            get { return this._selectedSprint; }
+            set
+            {
+                this._selectedSprint = value;
+                OnPropertyChanged("SelectedSprint");
+            }
+        }
+
+        public User SelectedAssignee
+        {
+            get { return this._selectedAssignee; }
+            set
+            {
+                this._selectedAssignee = value;
+                OnPropertyChanged("SelectedAssignee");
+            }
+        }
+
+        public Priority SelectedPriority
+        {
+            get { return this._selectedPriority; }
+            set
+            {
+                this._selectedPriority = value;
+                OnPropertyChanged("SelectedPriority");
+            }
+        }
+
+        public Status SelectedStatus
+        {
+            get { return this._selectedStatus; }
+            set
+            {
+                this._selectedStatus = value;
+                OnPropertyChanged("SelectedStatus");
+            }
+        }
+
+        public Project SelectedProject
+        {
+            get { return this._selectedProject; }
+            set
+            {
+                this._selectedProject = value;
+                OnPropertyChanged("SelectedProject");
+            }
+        }
+
+        public string SearchText
+        {
+            get { return this._searchText; }
+            set
+            {
+                this._searchText = value;
+                OnPropertyChanged("SearchText");
+            }
+        }
+
+        public ObservableCollection<Sprint> SprintList
+        {
+            get { return this._sprintList; }
+            set
+            {
+                this._sprintList = value;
+                OnPropertyChanged("SprintList");
+            }
+        }
+
+        public ObservableCollection<User> AssigneeList
+        {
+            get { return this._assigneeList; }
+            set
+            {
+                this._assigneeList = value;
+                OnPropertyChanged("AssigneeList");
+            }
+        }
+
+        public ObservableCollection<Priority> PriorityList
+        {
+            get { return this._priorityList; }
+            set
+            {
+                this._priorityList = value;
+                OnPropertyChanged("PriorityList");
+            }
+        }
+
+        public ObservableCollection<Status> StatusList
+        {
+            get { return this._statusList; }
+            set
+            {
+                this._statusList = value;
+                OnPropertyChanged("StatusList");
+            }
+        }
+
+        public ObservableCollection<Project> ProjectList
+        {
+            get { return this._projectList; }
+            set
+            {
+                this._projectList = value;
+                OnPropertyChanged("ProjectList");
+            }
         }
     }
 }
