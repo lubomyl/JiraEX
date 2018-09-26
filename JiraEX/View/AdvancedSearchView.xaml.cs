6c1dc782d75ab370ce24e6bd2e6ac3a792b3902d
lubomyl <31207065+lubomyl@users.noreply.github.com>
2018-09-26 10:10:51 +0200

    WIP: implementing advanced search - implementing view's list of issues

---
+++ JiraEX/View/AdvancedSearchView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using JiraEX.ViewModel.Navigation;
+using JiraRESTClient.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JiraEX.View
+{
+
+    public partial class AdvancedSearchView : UserControl
+    {
+
+        private AdvancedSearchViewModel _viewModel;
+
+        public AdvancedSearchView(JiraToolWindowNavigatorViewModel parent)
+        {
+            InitializeComponent();
+
+            this._viewModel = new AdvancedSearchViewModel(parent);
+        }
+
+        void IssueSelected_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            ListBox listBox = sender as ListBox;
+            Issue issue = listBox.SelectedItem as Issue;
+
+            this._viewModel.OnItemSelected(issue);
+        }
+    }
+}
