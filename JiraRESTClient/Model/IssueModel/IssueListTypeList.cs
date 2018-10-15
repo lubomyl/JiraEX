de3aa621d52fda7c613f41222ab5015ae39fda9b
lubomyl <31207065+lubomyl@users.noreply.github.com>
2018-10-15 07:02:25 +0200

    WIP: implementing linking of issues - issue link type model class

---
+++ JiraRESTClient/Model/IssueModel/IssueListTypeList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiraRESTClient.Model
+{
+    public class IssueListTypeList
+    {
+
+        public List<IssueLinkType> IssueLinkTypes { get; set; }
+
+    }
+}
